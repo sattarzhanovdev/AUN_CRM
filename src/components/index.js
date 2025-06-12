3f9a781a957bd4698c5d953a699d579e6bf16941
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-06-12 17:25:36 +0600

    push

---
+++ src/components/index.js
@@ -0,0 +1,52 @@
+import AddClient from './addClient'
+import AddExpense from './addExpense'
+import AddStock from './addStock'
+import AddTime from './addTime'
+import ClientsReport from './clientsReport'
+import ClientsTable from './clientsTable'
+import EditClient from './editClient'
+import ExpensesReport from './expensesReport'
+import ExpensesTable from './expensesTable'
+import FinancesReport from './financesReport'
+import FinancesTable from './financesTable'
+import MainReport from './mainReport'
+import Navbar from './navbar'
+import StockReport from './stockReport'
+import StockTable from './stockTable'
+import WorkersBenefit from './workersBenefit'
+import Kassa from './kassa'
+import Appointments from './appointments'
+import Cabinets from './cabinets'
+import AddProfit from './addProfit'
+
+export const Components = {
+  Navbar,
+
+  MainReport,
+
+  WorkersBenefit,
+  Kassa,
+  Appointments,
+  Cabinets,
+
+  ClientsReport,
+  ClientsTable,
+
+  FinancesReport,
+  FinancesTable,
+
+  ExpensesReport,
+  ExpensesTable,
+
+  AddClient,
+  AddExpense,
+  AddTime,
+  AddProfit,
+
+  EditClient,
+
+  StockReport,
+  StockTable,
+
+  AddStock
+}
