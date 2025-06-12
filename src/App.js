11f0b9536f0ff4ac6dfdb55c7a2bf3f29434cc5a
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-06-12 17:25:36 +0600

    push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.scss'
+import { Components } from './components'
+import MainRoutes from './routes'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/clients'
+
+function App() {
+  return (
+    <div>
+      <Components.Navbar />
+      <MainRoutes />
+    </div>
+  )
+}
+
+export default App
---
+++ src/api/index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export const API = {
+  // getClients: () => axios.get('/clients/clients/'),
+  // postClient: (data) => axios.post('/clients/clients/', data),
+  // updateClient: (id, data) => axios.put(`/clients/clients/${id}/`, data),
+  // getClientById: (id) => axios.get(`/clients/clients/${id}/`),
+  // getExpenses: () => axios.get('/clients/expenses/'),
+  // postExpenses: (data) => axios.post('/clients/expenses/', data),
+  // postProfit: (data) => axios.post('/clients/profit/', data),
+  // postStock: (data) => axios.post('/clients/stock/', data),
+  // getDailyExpenses: () => axios.get('/clients/expenses/stats/daily/'),
+  // getWorkers: () => axios.get('/clients/workers/'),
+  // getDailyClients: () => axios.get('/clients/today/'),
+  // getStocks: () => axios.get('/clients/stock/'),
+  // getMonthlyData: () => axios.get('/clients/stats/monthly/'),
+  // getStaffInfo: () => axios.get('/clients/stats/staff/'),
+  // getStaffInfoWeek: (week) => axios.get(`/clients/stats/staff/?week=${week}`),
+  // getCashInfo: () => axios.get('/clients/stats/cash-daily/')
+  getTransactions: () => axios.get('/transactions/'),
+  getTransactionsSummary: () => axios.get('/transactions/summary/'),
+  postTransactions: (data) => axios.post('/transactions/', data),
+  getTransactionById: (id) => axios.get(`/transactions/${id}/`),
+  updateTransaction: (id, data) => axios.put(`/transactions/${id}/`, data),
+  deleteTransaction: (id) => axios.delete(`/transactions/${id}/`),
+}
