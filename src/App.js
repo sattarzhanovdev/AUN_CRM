072778c80ab43690f32f646044428166fdfc4d62
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-06-12 18:24:24 +0600

    push

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.scss'
 import { Components } from './components'
 import MainRoutes from './routes'
 import axios from 'axios'
+import BarcodeScanner from './pages/code'
 
 axios.defaults.baseURL = 'https://auncrm.pythonanywhere.com/clients'
 
 function App() {
   return (
     <div>
-      <Components.Navbar />
-      <MainRoutes />
+      {/* <Components.Navbar />
+      <MainRoutes /> */}
+      <BarcodeScanner />
     </div>
   )
 }
