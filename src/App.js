25ac6aab564bf67fbe0570f5a58cbb7bea700b8b
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-06-17 15:57:38 +0600

    push

--- src/App.js
+++ src/App.js
@@ -5,11 +5,25 @@ import MainRoutes from './routes'
 import axios from 'axios'
 import BarcodeScanner from './pages/code'
 import { API } from './api'
+import { useNavigate } from 'react-router-dom'
 
 axios.defaults.baseURL = 'https://auncrm.pythonanywhere.com/clients'
 // axios.defaults.baseURL = 'http://127.0.0.1:8000/clients'
 
 function App() {
+  const nav = useNavigate()
+
+  const path = window.location.pathname
+  React.useEffect(() => {
+    if(path !== '/kassa'){
+      const code = prompt('Введите код доступа к админке')
+      if(code !== 'aunabdyrai'){
+        alert('Неверный код доступа')
+        nav('/kassa')
+      }
+    }
+  }, [path])
+  
   return (
     <div>
       <Components.Navbar />
